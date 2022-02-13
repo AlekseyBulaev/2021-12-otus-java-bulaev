268012b1d5abecb0f22368a6ecb969540461c66b
AlekseiBulaev <bulaev.a.u@gmail.com>
2022-02-13 23:50:32 +0300

    hw02-collections. PR-review

--- hw02-collections/src/main/java/ru/otus/java/pro/homework/CustomerReverseOrder.java
+++ hw02-collections/src/main/java/ru/otus/java/pro/homework/CustomerReverseOrder.java
@@ -1,14 +1,14 @@
 package ru.otus.java.pro.homework;
 
 
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.List;
 
 public class CustomerReverseOrder {
 
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
-    private final LinkedList<Customer> list = new LinkedList<>();
+    private final Deque<Customer> list = new LinkedList<>();
 
     public void add(Customer customer) {
         list.add(customer);
