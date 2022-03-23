d0e4a41ca04a1cc3751147c521764f90df94eee4
AlekseiBulaev <bulaev.a.u@gmail.com>
2022-03-23 21:26:43 +0300

    hw05-todo init commit

---
+++ hw05-todo/src/main/java/otus/listener/homework/HistoryListener.java
@@ -0,0 +1,18 @@
+package otus.listener.homework;
+
+import otus.listener.Listener;
+import otus.model.Message;
+import java.util.Optional;
+
+public class HistoryListener implements Listener, HistoryReader {
+
+    @Override
+    public void onUpdated(Message msg) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Optional<Message> findMessageById(long id) {
+        throw new UnsupportedOperationException();
+    }
+}
