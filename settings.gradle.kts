ff79bbc550d82b8a53cf4379e4875c306464afc4
AlekseiBulaev <bulaev.a.u@gmail.com>
2022-03-23 21:28:58 +0300

    hw06-gc addinc 04 homework

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,6 @@ rootProject.name = "2021-12-Java-Developer-Professional-Bulaev"
 include("hw01-gradle")
 include("hw02-collections")
 include("hw03-annotations")
+include("hw04-gc")
 include("hw06-atm")
 
