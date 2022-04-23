eefdaed2143586c5ad92bc5eb0305ac623a2c4ac
AlekseiBulaev <bulaev.a.u@gmail.com>
2022-04-24 01:18:47 +0300

    hw09-jdbc init commit

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,6 +2,12 @@ object Versions {
     const val guava = "31.0.1-jre"
     const val junit = "5.8.2"
     const val assertj = "3.22.0"
+    const val postgres = "42.3.4"
+    const val flyway = "8.5.9"
+    const val logback = "1.3.0-alpha14"
+    const val containerJUnit = "1.17.1"
+    const val containerPostgres = "1.17.1"
+    const val hikari = "5.0.1"
 }
 
 object Libs {
@@ -10,7 +16,13 @@ object Libs {
     val testImplementation = listOf(
         "org.assertj:assertj-core:${Versions.assertj}",
         "org.junit.jupiter:junit-jupiter-api:${Versions.junit}",
+        "org.testcontainers:junit-jupiter:${Versions.containerJUnit}",
+        "org.testcontainers:postgresql:${Versions.containerPostgres}",
     )
 
     val testRuntimeOnly = listOf("org.junit.jupiter:junit-jupiter-engine")
+    val postgres = "org.postgresql:postgresql:${Versions.postgres}"
+    val flyway = "org.flywaydb:flyway-core:${Versions.flyway}"
+    val logback = "ch.qos.logback:logback-classic:${Versions.logback}"
+    val hikari = "com.zaxxer:HikariCP:${Versions.hikari}"
 }
---
+++ hw09-jdbc/src/main/java/ru/otus/jdbc/mapper/EntitySQLMetaDataManager.java
@@ -0,0 +1,23 @@
+package ru.otus.jdbc.mapper;
+
+public class EntitySQLMetaDataManager implements EntitySQLMetaData{
+    @Override
+    public String getSelectAllSql() {
+        return null;
+    }
+
+    @Override
+    public String getSelectByIdSql() {
+        return null;
+    }
+
+    @Override
+    public String getInsertSql() {
+        return null;
+    }
+
+    @Override
+    public String getUpdateSql() {
+        return null;
+    }
+}
