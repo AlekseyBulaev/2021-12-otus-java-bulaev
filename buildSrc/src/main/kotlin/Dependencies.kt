b3ba7ed41f43f42751f8f32b63e35ca7740da0db
AlekseiBulaev <bulaev.a.u@gmail.com>
2022-05-06 23:37:06 +0300

    hw-10-jpql fixes

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,12 +2,6 @@ object Versions {
     const val guava = "31.0.1-jre"
     const val junit = "5.8.2"
     const val assertj = "3.22.0"
-    const val postgres = "42.3.4"
-    const val flyway = "8.5.9"
-    const val logback = "1.3.0-alpha14"
-    const val containerJUnit = "1.17.1"
-    const val containerPostgres = "1.17.1"
-    const val hikari = "5.0.1"
     const val mockito = "4.5.0"
     const val jackson = "2.13.2.2"
     const val postgres = "42.3.4"
@@ -50,8 +44,4 @@ object Libs {
         "org.testcontainers:postgresql:${Versions.containerPostgres}",
     )
     val testRuntimeOnly = listOf("org.junit.jupiter:junit-jupiter-engine")
-    val postgres = "org.postgresql:postgresql:${Versions.postgres}"
-    val flyway = "org.flywaydb:flyway-core:${Versions.flyway}"
-    val logback = "ch.qos.logback:logback-classic:${Versions.logback}"
-    val hikari = "com.zaxxer:HikariCP:${Versions.hikari}"
 }
