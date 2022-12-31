9d0ceb89c77d585849c0e248db3d52871f9882fe
AlekseiBulaev <bulaev.a.u@gmail.com>
2022-12-30 23:53:12 -0500

    hw-11-cache finishes

--- hw11-cache/build.gradle.kts
+++ hw11-cache/build.gradle.kts
@@ -1,9 +1,14 @@
 dependencies {
     implementation(Libs.postgres)
+    implementation(Libs.h2)
     implementation(Libs.flyway)
     implementation(Libs.logback)
     implementation(Libs.hikari)
+    implementation(Libs.hibernate)
+    implementation(Libs.javax)
+    implementation(Libs.lombok)
 
+    testImplementation(Libs.h2)
     Libs.testImplementation.forEach(::testImplementation)
     Libs.testRuntimeOnly.forEach(::testRuntimeOnly)
 }
