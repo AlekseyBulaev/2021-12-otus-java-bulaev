06bc9536de8f508fdb39a0208c44157967319c8f
AlekseiBulaev <bulaev.a.u@gmail.com>
2022-12-30 23:52:56 -0500

    hw-11-cache finishes

--- hw11-cache/src/main/java/ru/otus/cachehw/MyCache.java
+++ hw11-cache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -1,36 +1,42 @@
 package ru.otus.cachehw;
 
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.WeakHashMap;
 
 public class MyCache<K, V> implements HwCache<K, V> {
 //Надо реализовать эти методы
 
+    private final ArrayList<HwListener> listeners = new ArrayList<>();
     private final Map<K, V> cache = new WeakHashMap<>();
 
     @Override
     public void put(K key, V value) {
         cache.put(key, value);
+        listeners.forEach(hwListener -> hwListener.notify(key, value, "put"));
     }
 
     @Override
     public void remove(K key) {
         cache.remove(key);
+        listeners.forEach(hwListener -> hwListener.notify(key, null, "remove"));
     }
 
     @Override
     public V get(K key) {
-        return cache.get(key);
+        V value = cache.get(key);
+        listeners.forEach(hwListener -> hwListener.notify(key, value, "get"));
+        return value;
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-
+        listeners.add(listener);
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-
+        listeners.remove(listener);
     }
 }
